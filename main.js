ff387995af0c3b26a8b002d1b04929f31e098481
akabri972160007mohsen <akabri.mohsen.9721160007@gmail.com>
2020-12-06 10:37:47 +0330

    slide 5

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[9]);
-// document.all[10].textContent = 'Hello';
-// // console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
-////////////////////////////////////////////////////////////////
-// slide 4
-console.log(document.getElementById('article-head'));
-var headerTitle = document.getElementById('article-head');
-var header = document.getElementById('article-box-head');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'Goodbye';
-console.log(headerTitle.innerText);
-headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000';
-//////////////////
+
+
+var items = document.getElementsByClassName('article-box');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[2].style.fontWeight = 'bold';
+items[3].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 4; i < items.length; i++){
+  items[i].style.backgroundColor = '#e0c0c0';
+}
