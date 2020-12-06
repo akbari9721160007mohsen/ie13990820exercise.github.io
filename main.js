07e419c4d94655feb48c09f21e60c5b70f31f52b
akabri972160007mohsen <akabri.mohsen.9721160007@gmail.com>
2020-12-06 10:02:28 +0330

    slide 1

---
+++ main.js
@@ -0,0 +1,14 @@
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title =  123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[9]);
+document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
